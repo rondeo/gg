9489edc86bd39ee53c2bd049d4f688eac81adb6b
amarbgbg <50252471+amarMusic@users.noreply.github.com>
2019-06-19 00:16:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const devs = ['389090790984515594'];
+const devs = ['316672040356675585'];
 const db = require('quick.db');
 const premium = ['470896018603376640']
 const client = new Discord.Client();   
@@ -27,7 +27,7 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
 const dateFormat = require('dateformat'); 
 const pretty = require('pretty-ms') 
-const prefix = '=';
+const prefix = 'c';
 var table = require('table').table
 var ti={}  
 ,spee={}
